b93012e5d2125b63009aa1977473ec3427b77392
Kamil Nawrotkiewicz <kamil.nawrotkiewicz@icloud.com>
2018-06-15 11:33:10 +0200

    [Security] Add controller

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,48 @@
+<?php
+
+/*
+ * This file is part of the Symfony package.
+ *
+ * (c) Fabien Potencier <fabien@symfony.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+/**
+ * Controller used to manage the application security.
+ */
+class SecurityController extends AbstractController
+{
+
+    /**
+     * @Route("/login", name="security_login")
+     * @param AuthenticationUtils $helper
+     * @return Response
+     */
+    public function login(AuthenticationUtils $helper): Response
+    {
+        return $this->render('security/login.html.twig', [
+            // last username entered by the user (if any)
+            'last_username' => $helper->getLastUsername(),
+            // last authentication error (if any)
+            'error' => $helper->getLastAuthenticationError(),
+        ]);
+    }
+
+    /**
+     *
+     * @Route("/logout", name="security_logout")
+     */
+    public function logout(): void
+    {
+        throw new \Exception('This should never be reached!');
+    }
+}
