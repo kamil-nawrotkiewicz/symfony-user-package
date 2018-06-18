370e5f351ad2a6293144adf696083d94cee8fe19
Kamil Nawrotkiewicz <kamil.nawrotkiewicz@icloud.com>
2018-06-18 18:37:26 +0200

    [Form] expansion fields

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -14,13 +15,25 @@ class UserType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('fullName')
-            ->add('username')
-            ->add('email')
+            ->add('fullName', textType::class, [
+                'label' => 'form.fullName',
+                'translation_domain' => 'user'])
+            ->add('username',textType::class, [
+                'label' => 'form.username',
+                'translation_domain' => 'user'])
+            ->add('email',
+                textType::class, [
+                    'label' => 'form.email',
+                    'translation_domain' => 'user'])
             ->add('plainPassword', RepeatedType::class, array(
                 'type' => PasswordType::class,
-                'first_options'  => array('label' => 'Password'),
-                'second_options' => array('label' => 'Repeat Password'),
+                'first_options'  => [
+                    'label' => 'form.password',
+                    'translation_domain' => 'user'],
+                'second_options' => [
+                    'label' => 'form.plainPassword',
+                    'translation_domain' => 'user'],
+                'invalid_message' => 'Passwords must match.',
             ))
         ;
     }
